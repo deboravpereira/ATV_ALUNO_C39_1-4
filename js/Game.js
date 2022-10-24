032fd02e95734f623a3e51878ecb3521fd92ff98
deboravpereira <100171504+deboravpereira@users.noreply.github.com>
2022-10-24 13:26:11 -0300

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,73 @@
+class Game {
+  constructor() {}
+  //BP
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+  }
+  //BP
+  update(state) {
+    database.ref("/").update({
+      gameState: state
+    });
+  }
+
+  // TA
+  start() {
+    player = new Player();
+    playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+
+    car1 = createSprite(width / 2 - 50, height - 100);
+    car1.addImage("carro1", car1_img);
+    car1.scale = 0.07;
+
+    car2 = createSprite(width / 2 + 100, height - 100);
+    car2.addImage("carro2", car2_img);
+    car2.scale = 0.07;
+
+    cars = [car1, car2];
+  }
+
+  //BP
+  handleElements() {
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+  }
+
+  //SA
+  play() {
+    this.handleElements();
+    //Pegando as informações dos players
+    Player.getPlayersInfo();
+    
+    //Se os players não forem indefinidos
+    //mostramos a pista
+    if (allPlayers !== undefined) {
+      image(track, 0, -height * 5, width, height * 6);
+
+      //índice da matriz
+      var index = 0;
+      for (var plr in allPlayers) {
+        //use os dados do banco de dados para exibir os carros nas direções x e y
+        
+        
+        //adicione 1 ao índice de cada loop
+        
+      }
+
+      // manipule os eventos do teclado
+      if () {
+        
+      }
+
+      drawSprites();
+    }
+  }
+}
+
